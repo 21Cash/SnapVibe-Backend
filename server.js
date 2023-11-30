4937f854ae5e94fac4b77bb0cf9ecb12fb910b2f
21Cash <21ThousandProof@gmail.com>
2023-11-30 17:18:40 +0530

    Added Connects and disconnecting

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
@@ -6,6 +6,7 @@ import { create } from "domain";
 import { join } from "path";
 
 import dotenv from "dotenv";
+import { stringify } from "querystring";
 dotenv.config();
 
 const Port = process.env.PORT || 3000;
@@ -30,6 +31,30 @@ function getRoomName(socketId) {
   return IdToRoom[socketId];
 }
 
+function nameAlreadyPresent(roomName, curName) {
+  if (!rooms[roomName]) return false;
+  for (const id of rooms[roomName]) {
+    if (IdToName[id] == curName) return true;
+  }
+  return false;
+}
+
+function registerUser(socketId, userName, roomName) {
+  logEvent(null, "Register Success", { userName: userName });
+  IdToRoom[socketId] = roomName;
+  IdToName[socketId] = userName;
+}
+
+function unRegisterUser(id) {
+  const roomName = IdToRoom[id];
+  delete IdToRoom[id];
+  delete IdToName[id];
+  rooms[roomName].delete(id);
+  if (rooms[roomName].size === 0) {
+    delete rooms[roomName];
+  }
+}
+
 function createRoom(roomName) {
   if (!roomName) return false;
   if (!rooms[roomName]) {
@@ -42,25 +67,38 @@ function createRoom(roomName) {
   }
 }
 
+function emitRoomList(roomName) {
+  io.to(roomName).emit("room_list", {
+    list: [...rooms[roomName]].map((id) => IdToName[id]),
+  });
+}
+
 function joinRoom(roomName, userName, socket) {
-  console.log(`Req To Join ${roomName}`);
+  logEvent(socket, "JOINROOM", { userName: userName });
+  if (nameAlreadyPresent(roomName, userName) || userName == "SERVER") {
+    socket.emit("room_join_failed", {
+      msg: "Username already exists. Use different Username",
+    });
+    return false;
+  }
   const duplicate = rooms[roomName] && rooms[roomName].has(socket.id);
-  console.log(`Is Duplic : ${duplicate}`);
   if (!rooms[roomName] || rooms[roomName].has(socket.id)) {
-    console.log(`Room Not Present`);
-    socket.emit("room_join_fail", { msg: "Room Join Failed." });
+    console.log(`Room not present or already exists`);
+    socket.emit("room_join_failed", {
+      msg: "Room Join Failed. Room Not Present.",
+    });
     return false;
   }
   socket.join(roomName);
   rooms[roomName].add(socket.id);
-  IdToRoom[socket.id] = roomName;
-  IdToName[socket.id] = userName;
+  registerUser(socket.id, userName, roomName);
   console.log(`Join Room ${roomName} Success By User ${userName} `);
   socket.emit("room_join_success", { roomName: roomName });
   io.to(roomName).emit("user_joined", {
     id: socket.id,
     userName: userName,
   });
+  emitRoomList(roomName);
   return true;
 }
 
@@ -69,11 +107,12 @@ function leaveRoom(socket) {
   if (!IdToRoom[id]) return;
   const roomName = IdToRoom[id];
   const userName = IdToName[id];
-  delete IdToRoom[id];
-  io.to(roomName).emit("user_disconnect", {
+  unRegisterUser(socket.id);
+  io.to(roomName).emit("user_left", {
     id: socket.id,
     userName: userName,
   });
+  emitRoomList(roomName);
   socket.leave(roomName);
 }
 
@@ -110,15 +149,34 @@ io.on("connection", (socket) => {
     joinRoom(roomName, userName, socket);
   });
 
-  socket.on("leave_room", (data) => {});
+  socket.on("leave_room", (data) => {
+    logEvent(socket, "DISCONNECT", "Disconnected.");
+    leaveRoom(socket);
+  });
 
   socket.on("send_message", (data) => {
+    logEvent(socket, "send_message", data);
     const msg = data.msg;
-    socket
-      .to(getRoomName(socket.id))
-      .emit("receive_message", { senderId: socket.id, msg: msg });
+    io.to(getRoomName(socket.id)).emit("receive_message", {
+      senderId: socket.id,
+      sender: IdToName[socket.id],
+      msg: msg,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    logEvent(socket, "DISCONNECT", "Disconnected.");
+    leaveRoom(socket);
   });
 });
+
+const logEvent = (socket, eventName, data) => {
+  console.log(
+    `[${
+      socket && IdToName[socket.id]
+    }], Event : ${eventName}, Data : ${stringify(data)}`
+  );
+};
 
 /*
 Server Emits
